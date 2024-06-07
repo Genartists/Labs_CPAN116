cbcc274fdd62e6babd2e1a220512f8da4bb0f0cb
Hao Le <n01605830@humbermail.ca>
2024-06-06 20:06:39 -0400

    update 2

--- src/main.java
+++ src/main.java
@@ -4,23 +4,22 @@ public class main {
     public static void main(String[] args) {
         FactorialCalculator myFact = new FactorialCalculator();
 
-        try (Scanner sc = new Scanner(System.in)) {
-            while (true) {
-                System.out.print("Enter a number (or exit to quit): ");
-                if (sc.hasNextInt()) {
-                    int numInput = sc.nextInt();
-                    myFact.calculateFactorial(numInput);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to the Factorial Calculator!");
+        while (true) {
+            System.out.print("Enter a number (or exit to quit): ");
+            if (sc.hasNextInt()) {
+                int numInput = sc.nextInt();
+                myFact.calculateFactorial(numInput);
+            } else {
+                String exit = sc.next();
+                if (exit.equalsIgnoreCase("exit")) {
+                    System.out.println("Thank for using the program!");
+                    break;
                 } else {
-                    String next = sc.next();
-                    String exit = "exit";
-                    if (next.equalsIgnoreCase(exit)) {
-                        System.out.println("Thank for using the program!");
-                        break;
-                    }
+                    System.out.println("Please enter a valid number");
                 }
             }
-        } catch (Exception e) {
-            System.out.println("Please enter a valid number" + e.getMessage());
         }
     }
 }
