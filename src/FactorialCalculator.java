e661f8698b18c45eb0eb21a74fe23ba441f646da
Hao Le <n01605830@humbermail.ca>
2024-06-06 20:17:52 -0400

    new update

--- src/FactorialCalculator.java
+++ src/FactorialCalculator.java
@@ -2,14 +2,18 @@ class FactorialCalculator {
     private long num;
     private long res;
 
+    
     public void calculateFactorial(long num) {
-        this.num = num;
-        this.res = 1;
+        this.num = num; // assign the input to the class variable
+        this.res = 1; // initialize the result to 1
+
+        // check if the input is negative or 0. If positive then calculate the factorial
         if (num < 0) {
             System.out.println("Factorial of negative number is undefined");
         } else if (num == 0) {
             System.out.println("Factorial of 0 is 1");
         } else {
+            // for loop to calculate the factorial
             for (int i = 1; i <= num; i++) {
                 res *= i;
             }
--- src/main.java
+++ src/main.java
@@ -2,21 +2,30 @@ import java.util.Scanner;
 
 public class main {
     public static void main(String[] args) {
+        // Create an instance of the FactorialCalculator class
         FactorialCalculator myFact = new FactorialCalculator();
 
+        // Create prompt to get user input
         Scanner sc = new Scanner(System.in);
         System.out.println("Welcome to the Factorial Calculator!");
+
+        // Infinite loop to keep asking for input
         while (true) {
             System.out.print("Enter a number (or exit to quit): ");
+            // outer if statement check if user input is an integer
             if (sc.hasNextInt()) {
                 int numInput = sc.nextInt();
-                myFact.calculateFactorial(numInput);
-            } else {
+                myFact.calculateFactorial(numInput); // call the calculateFactorial method
+            }
+            // exit the program if user types "exit"
+            else {
                 String exit = sc.next();
                 if (exit.equalsIgnoreCase("exit")) {
                     System.out.println("Thank for using the program!");
                     break;
-                } else {
+                }
+                // raise error if user input is not an integer
+                else {
                     System.out.println("Please enter a valid number");
                 }
             }
